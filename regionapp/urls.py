7e1d2c7c4b8aea463231d15e214c88523787ca92
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-21 02:23:06 +0300

    feat: include path to post,single region and search

--- regionapp/urls.py
+++ regionapp/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path, include
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.landing, name='landing'),
+    path('login/',views.signin,name='login'),
+    path('register/',views.register,name='register'),
+    path('logout/',views.signout,name='logout'),
+    path('index/',views.index,name='index'),
+    path('new-region/', views.create_region, name='new-region'),
+    path('search/', views.search_business, name='search'),
+    path('profile/<username>', views.profile, name='profile'),
+    path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('single_region/<region_id>', views.single_region, name='single-region'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
