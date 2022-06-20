f590504fc037247bdf9bf2bc6562b54254fcdec1
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-20 11:53:35 +0300

    feat: include manage.py file to project root

---
+++ regionapp/models.py
@@ -0,0 +1,21 @@
+# import email
+# from django.db import models
+
+# # Create your models here.
+# class NeighbourHood(models.Model)
+#     name=models.TextField
+#     locaton=
+#     Occupants_count
+#     admin=models.ForeignKey(Admin)
+
+# class User(models.Model):
+#     name=
+#     id=
+#     NeighbourHood=
+#     email
+
+# class Business(models.Model):
+#     name=
+#     user=
+#     NeighbourHood=
+#     email=
